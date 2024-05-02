ecec6249d4cf3919c940809b4d6585ba28e69d13
boneandrea <bone.andrea.anthrax@gmail.com>
2024-05-02 09:11:46 +0900

    apply php-cs-fixer

--- back/cal.php
+++ back/cal.php
@@ -1,37 +1,38 @@
 <?php
-require(__DIR__."/S.php");
-$x=new S();
 
-$body=file_get_contents("php://input");
-$json=json_decode($body,true);
-error_log(print_r($json,true));
+require(__DIR__."/simulator.php");
 
-$options=$json["data"] ?? [];
+$body = file_get_contents("php://input");
+$json = json_decode($body, true);
+error_log(print_r($json, true));
 
-error_log(print_r($options,true));
+$options = $json["data"] ?? [];
 
-$rate_rakuten=(float)($options["r"] ?? 1.1);
-$rate_sony=(float)($options["s"] ?? 1.1);
-$deposit_rakuten=(float)($options["d"] ?? 250);
-$stop_year=(int)($options["year"] ?? 57);
+error_log(print_r($options, true));
 
-$withdraw_rakuten=(float)($options["R"] ?? 50);
-$withdraw_sony=(float)($options["S"] ?? 28);
-$rate_later=$options["rate_later"] ?? [
-    "sony"=>1.06,
-    "rakuten"=>1.06,
+$rate_rakuten = (float)($options["r"] ?? 1.1);
+$rate_sony = (float)($options["s"] ?? 1.1);
+$deposit_rakuten = (float)($options["d"] ?? 250);
+$stop_year = (int)($options["year"] ?? 57);
+
+$withdraw_rakuten = (float)($options["R"] ?? 50);
+$withdraw_sony = (float)($options["S"] ?? 28);
+$rate_later = $options["rate_later"] ?? [
+    "sony" => 1.06,
+    "rakuten" => 1.06,
 ];
-$year_change_rate=$options["year_change_rate"] ?? 0;
+$year_change_rate = $options["year_change_rate"] ?? 0;
 
-$sum_draw_per_year=[];
-$x=new S(isCli: (php_sapi_name() === "cli"),
-         rate_sony:$rate_sony,
-         rate_rakuten:$rate_rakuten,
-         rate_later:$rate_later,
-         year_change_rate:$year_change_rate
+$sum_draw_per_year = [];
+$x = new Simulator(
+    isCli: (php_sapi_name() === "cli"),
+    rate_sony:$rate_sony,
+    rate_rakuten:$rate_rakuten,
+    rate_later:$rate_later,
+    year_change_rate:$year_change_rate
 );
-$result=[];
-$result["sony"]=$x->cal(
+$result = [];
+$result["sony"] = $x->cal(
     json: true,
     name: "ソニー",
     asset:0,
@@ -42,7 +43,7 @@ $result["sony"]=$x->cal(
 );
 
 
-$result["rakuten"]=$x->cal(
+$result["rakuten"] = $x->cal(
     json: true,
     paid_sum:1400,
     name: "楽天",
@@ -55,7 +56,7 @@ $result["rakuten"]=$x->cal(
 
 $x->dump_sum_draw();
 
-if(php_sapi_name() !== "cli"){
+if(php_sapi_name() !== "cli") {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
     header("Content-type: application/json");
